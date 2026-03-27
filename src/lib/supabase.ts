8e90eba0510435ced517c7afd25e533b32775ec6
user-admin123 <600001raj@gmail.com>
2026-03-27 10:19:55 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://nsijuvwgmdnqvbejytyt.supabase.co1";
+const supabaseUrl = "https://nsijuvwgmdnqvbejytyt.supabase.co";
 const supabaseKey = "sb_publishable_begu2_8_hKwdVadii73E4Q_KUWma338";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
